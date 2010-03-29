fe2393668855467fc7979a33f006e5705ad04d2e
Mark A. Matienzo <mark@matienzo.org>
2010-03-29 16:53:21 -0400

    Adding example config file

---
+++ at-config-sample.php
@@ -0,0 +1,9 @@
+<?php
+/* Basic configuration
+*/
+define('ATDB_USER', 'atuser');
+define('ATDB_PASS', 'password');
+define('ATDB_HOST', 'atserver.example.com');
+define('ATDB_DATABASE', 'toolkit');
+
+?>
